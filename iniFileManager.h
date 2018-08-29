648f3c5b95da3d1cc5e3da021284779d8ba31b33
frenk <francesco.vannoni@stud.unifi.it>
2018-08-29 13:50:36 +0200

    template and methods to work with section, parameters and values added

--- iniFileManager.h
+++ iniFileManager.h
@@ -6,27 +6,35 @@
 #define LABORATORIO_INIFILEMANAGER_H
 
 #include <string>
+#include <map>
 
 using namespace std;
+template <typename T>
 class iniFileManager {
 public:
     iniFileManager(string fileName);
-    string getStringElement (string section, string keyName);
-    int getIntElement (string section, string keyName);
-    float getFloatElement (string section, string keyName);
 
-    void setStringElement (string section, string keyName, string newElement);
-    void setFloatElement (string section, string keyName, float newElement);
-    void setBoolElement (string section, string keyName, bool  newElement);
+    T& getValues (string section, string parameter);
 
+    void addValues (string section, string parameter, T newValue);
 
+    bool addSection (string sectionName);
+    string getSections();
 
+    bool addParameter(string parameterName);
+    void getParameter();
+
+    bool deleteInt(string section, string parameter, int valueDeleted);
+    bool deleteString(string section, string parameter, string valueDeleted);
+    bool deleteBool(string section, string parameter, bool valueDeleted);
+    bool deleteFloat(string section, string parameter, float valueDeleted);
 
     virtual ~iniFileManager();
 
 
 private:
     string fileName;
+    map<string,map<string,T>> file;
 };
 
 
