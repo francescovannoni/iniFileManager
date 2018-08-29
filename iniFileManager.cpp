602e97ca2e2c4c5bb764ba1fd4a662992e8dea53
frenk <francesco.vannoni@stud.unifi.it>
2018-08-29 13:50:50 +0200

    template and methods to work with section, parameters and values added

--- iniFileManager.cpp
+++ iniFileManager.cpp
@@ -6,10 +6,42 @@
 using namespace std;
 
 iniFileManager::iniFileManager(string filenName) {
-
+this->fileName = filenName;
 }
 
 iniFileManager::~iniFileManager() {
+
+}
+
+string iniFileManager::getStringElement(string section, string parameter) {
+    return std::__cxx11::string();
+}
+
+int iniFileManager::getIntElement(string section, string parameter) {
+    return 0;
+}
+
+float iniFileManager::getFloatElement(string section, string parameter) {
+    return 0;
+}
+
+bool iniFileManager::getBool(string section, string parameter) {
+    return false;
+}
+
+void iniFileManager::setStringElement(string section, string parameter, string newElement) {
+
+}
+
+void iniFileManager::setIntElement(string section, string parameter, int newElement) {
+
+}
+
+void iniFileManager::setFloatElement(string section, string parameter, float newElement) {
+
+}
+
+void iniFileManager::setBoolElement(string section, string parameter, bool newElement) {
 
 }
 
